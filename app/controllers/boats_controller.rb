4f6d7c93b4197776f73daa88e7c5bca85ece1fbf
Kevin Ilunga <ayub0589@gmail.com>
2024-05-27 17:19:34 +0200

    edit & up

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,15 @@
 class BoatsController < ApplicationController
+  def edit
+    @boats = boats.find(params[:id])
+  end
+
+  def update
+    @boats = boats.find(params[:id])
+
+    if @boat.update(boat_params)
+      redirect_to boat_path(@boat)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
