65f89297b9cd81b4527b408e0e524fbc3b4a2392
Aline GOMES LOPES <aline.gl21@gmail.com>
2024-05-28 12:19:31 +0200

    Add new and create booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+  def new
+    @boat = Boat.find(params[:boat_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @boat = Boat.find(params[:boat_id])
+    @user = user.find(params[:user_id])
+    @booking = Booking.new(bookings_params)
+    @booking.boat = @boat
+    @booking.user = @user
+    @booking.save
+    redirect_to boat_bookings_path(@booking.boat_id)
+  end
+
+  def bookings_params
+    params.require(:booking).permit(:begin_date, :end_date, :status, :rating, :boat_id, :user_id)
+  end
 end
