3a40e6e21a9ea078cb3b71847c51677cbca6c7a2
Kevin Ilunga <ayub0589@gmail.com>
2024-05-29 11:40:18 +0200

    pics added to boat & user

---
+++ db/migrate/20240529093345_add_pictures_to_users.rb
@@ -0,0 +1,5 @@
+class AddPicturesToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :avatar_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_091656) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_093345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_091656) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.string "picture_url"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
@@ -46,6 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_091656) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
